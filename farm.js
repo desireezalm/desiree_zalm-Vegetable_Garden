eb9967fd74c606fd981ebc4d94fdcb087c35408b
Desiree Zalm <d.zalm@outlook.com>
2022-01-07 17:10:05 +0100

    Re-named file with javascript functions to farm.js; added farm.test.js and copied tests from assignment; initialised npm package for Jest testing.

---
+++ farm.js
@@ -0,0 +1,20 @@
+// ASSIGNMENT: VEGETABLE GARDEN
+
+const getYieldForPlant = () => {
+    // CODE
+};
+
+const getYieldForCrop = () => {
+    // CODE
+};
+
+const getTotalYield = () => {
+    // CODE
+};
+
+module.exports = {
+    getYieldForPlant, 
+    getYieldForCrop, 
+    getTotalYield
+};
+
